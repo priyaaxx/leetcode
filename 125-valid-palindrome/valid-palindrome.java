9f6bdde5c693c331fc7b448028522b58c3855ac2
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2024-07-06 11:43:50 +0530

    Time: 266 ms (5.07%) | Memory: 45.9 MB (10.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,8 +4,8 @@ class Solution {
         int i = 0;
         int j = s.length() - 1;
         while(i<j){
-            char char1 = s.charAt(i);
-            char char2 = s.charAt(j);
+            int char1 = s.charAt(i);
+            int char2 = s.charAt(j);
             System.out.println(char1);
             System.out.println(char2);
 
@@ -14,6 +14,12 @@ class Solution {
             } else if(!(Character.isLetter(char2)|| Character.isDigit(char2))){
                 j--;
             } else{
+                if(char1>='a' && char1<='z'){
+                    char1 = char1 - (int)'a';
+                }
+                if(char2>='a' && char2<='z'){
+                    char2 = char2 - (int)'a';
+                }
                 if(char1 == char2){
                     i++;
                     j--;
